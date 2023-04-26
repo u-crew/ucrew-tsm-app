41dcda3ae0dcd849ece08ed28b3939f811ce1d02
u-crew <129836422+u-crew@users.noreply.github.com>
2023-04-26 17:52:41 +1000

    fix ssrf demo vuln as well - albeit this was not working for obvious uday-type mistakes

--- src/pages/api/ssrf.ts
+++ src/pages/api/ssrf.ts
@@ -1,11 +1,7 @@
-import * as  express from 'express';
-import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next'
-const app = express();
-const port = 3000;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const imgUrl = req.body.imgUrl;
-  const imageReq = await axios.get(imgUrl);
-  res.send(imageReq.data);
+  console.log(`Image URL is ${req.body.imgUrl}`);
+  res.status(200);
 }
