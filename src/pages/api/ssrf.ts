5273d3ac33fdbff858599e05ed6ceb11fd601f66
u-crew <129836422+u-crew@users.noreply.github.com>
2023-06-23 11:17:13 +1000

    new version v0.1.10

--- src/pages/api/ssrf.ts
+++ src/pages/api/ssrf.ts
@@ -1,7 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const PASSWORD = "123";
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const imgUrl = req.body.imgUrl;
-  console.log(`Image URL is ${req.body.imgUrl}`);
+  console.log(`Image URL is ${req.body.imgUrl}. password: ${PASSWORD}`);
   res.status(200);
 }
