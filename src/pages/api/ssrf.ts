e55d5c43d486de563658fb44f5deb429139124f5
Uday Korlimarla <129836422+u-crew@users.noreply.github.com>
2023-07-03 10:48:50 +0530

    introduce XSS and SSRF

--- src/pages/api/ssrf.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-const PASSWORD = "123";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const imgUrl = req.body.imgUrl;
-  console.log(`Image URL is ${req.body.imgUrl}. password: ${PASSWORD}`);
-  res.status(200);
-}
